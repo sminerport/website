aa9a3f1d93ceb899ff1ff311a4448a830b0bef4b
Scott <scott.miner.data.scientist@gmail.com>
2020-05-29 03:43:13 -0600

    add regression modeling in predictive analytics post, add mathjax, add js pages that don't work, update footer.html with mathjax snippet

---
+++ static/js/test.js
@@ -0,0 +1,7 @@
+function alertColor() {
+  var result = "<span class='fa fa-info-circle'></span>&nbsp;alert-info";
+  document.getElementById("toInfo").innerHTML = result; 
+  document.getElementById("myButton").style.backgroundColor = "#d9edf7";
+  document.getElementById("myButton").style.color = "#3a87ad";
+  document.getElementById("myButton").style.borderColor = "#bce8f1";
+}
